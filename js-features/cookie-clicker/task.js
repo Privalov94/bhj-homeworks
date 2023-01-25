8291666eaaa95c77de6a6014cd114bceef723c13
Privalov94 <arprivalov110594@gmail.com>
2023-01-25 08:12:47 +0500

    first commit cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,14 @@
 let clicks = 0;
+let flag = 180;
 const image = document.getElementById("cookie");
 const clickerStatus = document.getElementById("clicker__counter");
 
-function start() {
-    clickerStatus.textContent = clicks++; 
+image.onclick = function() {
+    clickerStatus.textContent = clicks++;
+    if(flag < image.width) {
+        image.width = image.width - 20;
+    } else {
+        image.width = 200;
+    }
 }
-
-image.onclick = start;
 
