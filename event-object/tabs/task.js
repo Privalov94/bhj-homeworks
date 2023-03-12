b3d9e08bac2b6e8e8c48d2b6a8f78288c5efce4b
Privalov94 <arprivalov110594@gmail.com>
2023-03-12 11:16:45 +0500

    first commit tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,25 +1,50 @@
 
-let tabs = function () {
-    let tab = Array.from(document.querySelectorAll('.tab'));
-    let tabContent = document.querySelectorAll('.tab__content');
-    let tabIndex;
-
-    tab.forEach(item => {
-        item.addEventListener('click', selectTab);
-    })
-
-    function selectTab() {
-        tab.forEach((item, index) => {
-            item.classList.remove('tab_active');
-            tabIndex = index
-        })
-        this.classList.add('tab_active')
-
-        tabContent.forEach(value => {
-            value.classList.remove('tab__content_active')
-        })
-        tabContent[tabIndex].classList.add('tab__content_active')
+class ItcTabs {
+    constructor(target, config) {
+      const defaultConfig = {};
+      this._config = Object.assign(defaultConfig, config);
+      this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
+      this._elButtons = this._elTabs.querySelectorAll('.tab');
+      this._elPanes = this._elTabs.querySelectorAll('.tab__content');
+      this._eventShow = new Event('tab.itc.change');
+      this._init();
+      this._events();
     }
-}
+    _init() {
+      this._elTabs.setAttribute('role', 'tablist');
+      this._elButtons.forEach((el, index) => {
+        el.dataset.index = index;
+        el.setAttribute('role', 'tab');
+        this._elPanes[index].setAttribute('role', 'tabpanel');
+      });
+    }
+    show(elLinkTarget) {
+      const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+      const elLinkActive = this._elTabs.querySelector('.tab_active');
+      const elPaneShow = this._elTabs.querySelector('.tab__content_active');
+      if (elLinkTarget === elLinkActive) {
+        return;
+      }
+      elLinkActive ? elLinkActive.classList.remove('tab_active') : null;
+      elPaneShow ? elPaneShow.classList.remove('tab__content_active') : null;
+      elLinkTarget.classList.add('tab_active');
+      elPaneTarget.classList.add('tab__content_active');
+      this._elTabs.dispatchEvent(this._eventShow);
+      elLinkTarget.focus();
+    }
+    showByIndex(index) {
+      const elLinkTarget = this._elButtons[index];
+      elLinkTarget ? this.show(elLinkTarget) : null;
+    };
+    _events() {
+      this._elTabs.addEventListener('click', (e) => {
+        const target = e.target.closest('.tab');
+        if (target) {
+          e.preventDefault();
+          this.show(target);
+        }
+      });
+    }
+  }
 
-tabs()
+  new ItcTabs('.tabs');
