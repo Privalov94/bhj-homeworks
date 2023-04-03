0913b9faa938be3e76da841a12c863586f2b8c5a
Privalov94 <arprivalov110594@gmail.com>
2023-04-03 21:15:59 +0500

    first commit progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,13 +1,16 @@
-const file = document.getElementById('file');
-const progress = document.getElementById( 'progress' );
 
-document.forms.form.addEventListener('sumbit', (e) => {
-    e.preventDefault();
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
+const file = document.getElementById('file');
+
+
+form.addEventListener('sumbit', (event) => {
+    event.preventDefault();
 
     const xhr = new XMLHttpRequest();
 
     xhr.addEventListener('readystatechange', ()=> {
-        if(xhr.readyState === xhr.DONE) {
+        if(xhr.readyState === xhr.DONE && xhr.status === 200) {
             xhr.upload.onprogress = function() {
                 progress.value = 0.7;
             }
@@ -17,7 +20,8 @@ document.forms.form.addEventListener('sumbit', (e) => {
     const formData = new FormData();
     formData.append('file', file.files[0]);
 
-    xhr.open('GET','https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.open('POST','https://students.netoservices.ru/nestjs-backend/upload');
 
     xhr.send(formData);
+    
 })
