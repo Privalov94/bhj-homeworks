2da491a5d0412e69186a284ba555a4fc14210dff
Privalov94 <arprivalov110594@gmail.com>
2023-04-10 16:12:28 +0500

    first commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,38 +1,64 @@
 const signin = document.getElementById('signin');
 const form = document.getElementById('signin__form');
-const control = document.querySelectorAll('.control');
 const btn = document.querySelector('.btn')
+const row = document.querySelectorAll('.row');
+const id = document.getElementById('user_id');
+const welcome = document.querySelector('.welcome');
 
 signin.classList.add('signin_active');
 
-const input = control.forEach(item => {
-    item.addEventListener('change', (event) => {
-        event.target
-    })
-})
 
 const forms = () => {
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const xhr = new XMLHttpRequest()
-        xhr.addEventListener('load', () => {
-            if(xhr.readyState === xhr.DONE) {
-            }
+    const xhr = new XMLHttpRequest();
+
+    row.forEach(item => {
+        item.addEventListener('change', (event) => {
+            event.target;
         })
-        const formData = new FormData(input);
-        xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-        xhr.send(formData);
     })
-    
+
+    const formData = new FormData(form);
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.send(formData);
+
+    xhr.addEventListener('load', function () {
+        if (xhr.readyState === xhr.DONE) {
+            let data = JSON.parse(xhr.response);
+            localStorage.setItem('user_id', data.user_id);
+            if (data.success === false) {
+                alert('Неверный логин/пароль');
+                welcome.classList.remove('welcome_active');
+            } else if (data.success === true) {
+                welcome.textContent += data.user_id;
+                welcome.classList.add('welcome_active');
+            }
+        }
+
+    })
+}
+
+const userGreetings = () => {
+    const result = localStorage.getItem('user_id');
+    if (result === 'undefined') {
+        welcome.classList.remove('welcome_active');
+    } else if (result) {
+        welcome.textContent += result;
+        welcome.classList.add('welcome_active');
+    } else {
+        return
+    }
 }
 
 const btnForms = () => {
-    btn.addEventListener('click', () => {
-        forms()
-    })
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        forms();
+    });
 }
 
-btnForms()
+
+btnForms();
+userGreetings();
 
 
 
